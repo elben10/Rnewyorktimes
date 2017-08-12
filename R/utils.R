c760ae75d545fc157341eda7ac5321e6d5e09955
Jakob Jul Elben <elbenjakobjul@gmail.com>
2017-08-12 19:01:23 +0200

    Update token helper so a token only can be set using nyt_token

--- R/utils.R
+++ R/utils.R
@@ -6,18 +6,12 @@ nyt_token <- function() {
   token_env$token <- rstudioapi::askForPassword("Please enter New York Times API token")
 }
 
-is_token_set <- function(token) {
+is_token_set <- function() {
 
-  if (!is_missing(token)) {
-    if (!is_character(token)) {
-      abort("token must be a string")
-    }
+  if (is_empty(token_env$token)) {
+    abort("A token must either be set using the nyt_token function")
   }
-
-  if (is_empty(token_env$token) & is_missing(token)) {
-    abort("A token must either be set as the token argument or by the nyt_token function")
-  }
-  if (is_missing(token)) token_env$token else token
+  token_env$token
 }
 
 is_whole_number <- function(x) {
