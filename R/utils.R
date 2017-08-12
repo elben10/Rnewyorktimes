fc5c122df985ef8ae7a2c473bdd64a883e8624fa
Jakob Jul Elben <elbenjakobjul@gmail.com>
2017-08-12 17:58:08 +0200

    token set helper

--- R/utils.R
+++ R/utils.R
@@ -1,7 +1,20 @@
-token <- rlang::new_environment()
+token_env <- rlang::new_environment()
 NULL
 
 #' @export
 nyt_token <- function() {
-  token$token <- rstudioapi::askForPassword("Please enter New York Times API token")
+  token_env$token <- rstudioapi::askForPassword("Please enter New York Times API token")
+}
+
+is_token_set <- function(token) {
+
+  if (!is_missing(token)) {
+    if (!is_character(token)) {
+      abort("token must be a string")
+    }
+  }
+
+  if (is_empty(token_env$token) & is_missing(token)) {
+    abort("A token must either be set as the token argument or by the nyt_token function")
+  }
 }
