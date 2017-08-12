203f286ce097e4a753513f8b6a9cd16670d57655
Jakob Jul Elben <elbenjakobjul@gmail.com>
2017-08-12 19:11:37 +0200

    fix type and add is_json and request_failed helpers

--- R/utils.R
+++ R/utils.R
@@ -9,7 +9,7 @@ nyt_token <- function() {
 is_token_set <- function() {
 
   if (is_empty(token_env$token)) {
-    abort("A token must either be set using the nyt_token function")
+    abort("A token must be set using the nyt_token function")
   }
   token_env$token
 }
@@ -21,4 +21,22 @@ is_whole_number <- function(x) {
 sys_time <- function(numeric = FALSE, format = "%Y-%m-%d %H:%M:%S %Z") {
   res <- format(Sys.time(), format)
   if (numeric) as.numeric(res) else res
+}
+
+is_json <- function(resp) {
+  if (http_type(resp) != "application/json") {
+    abort(sprintf("API did not return application/json, but %s",
+                  http_type(resp)))
+  }
+}
+
+request_failed <- function(resp, parsed) {
+  if (http_error(resp)) {
+    abort(sprintf(
+      "New York Times API request failed [%s]\n%s\n<%s>",
+      status_code(resp),
+      parsed$message,
+      parsed$documentation_url
+    ))
+  }
 }
